cf4599e51938a9dbe374c6ea17ff72bc6d7b0d1a
tsbslteisrhy <tsbslteisrhy@naver.com>
2020-05-28 14:55:08 +0900

    2020/05/28 Ch15 교재 예제 추가

---
+++ Ch15/src/p739/HashSetExample2.java
@@ -0,0 +1,16 @@
+package p739;
+
+import java.util.*;
+
+import p738.Member;
+
+public class HashSetExample2 {
+	public static void main(String[] args) {
+		Set<Member> set = new HashSet<Member>();
+		
+		set.add(new Member("È«±æµ¿", 30));
+		set.add(new Member("È«±æµ¿", 30));
+		
+		System.out.println("ÃÑ °´Ã¼¼ö: "+set.size());
+	}
+}
---
+++ Ch15/src/p768/Coin.java
@@ -0,0 +1,13 @@
+package p768;
+
+public class Coin {
+	private int value;
+	
+	public Coin(int value) {
+		this.value = value;
+	}
+	
+	public int getValue() {
+		return value;
+	}
+}
